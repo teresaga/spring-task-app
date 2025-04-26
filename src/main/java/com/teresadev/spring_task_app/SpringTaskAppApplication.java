2c008e49949faf4d39a885e209053e374a8a1396
Teresa <teresagalaviza@gmail.com>
2025-04-25 22:52:40 -0700

    First commit: Task entity added

---
+++ src/main/java/com/teresadev/spring_task_app/SpringTaskAppApplication.java
@@ -0,0 +1,13 @@
+package com.teresadev.spring_task_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaskAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/teresadev/spring_task_app/entity/Task.java
@@ -0,0 +1,42 @@
+package com.teresadev.spring_task_app.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name="task")
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+    @Column(name = "is_done")
+    private boolean isDone;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @Column(name = "create_at")
+    private Date createAt;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @Column(name = "end_date")
+    private Date endDate;
+
+    /*
+    @Column(name = "used_id")
+    private int usedId;*/
+}
